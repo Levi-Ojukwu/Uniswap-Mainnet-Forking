d75596b5474250395f07393680de806eae536f80
Levi-Ojukwu <ojukwulevichinedu@gmail.com>
2024-02-26 20:56:56 +0100

    Second ccommit with interactions

--- contracts/IUniswap.sol
+++ contracts/IUniswap.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+import "";
+
 pragma solidity ^0.8.20;
 interface IUniswap{
     function swapExactTokensForTokens(
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,46 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const MAINET_ALCHEMY_API_KEY_URL = process.env.MAINET_ALCHEMY_API_KEY_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+if (
+  !(
+    ALCHEMY_API_KEY_URL ||
+    PRIVATE_KEY ||
+    ETHERSCAN_API_KEY ||
+    MAINET_ALCHEMY_API_KEY_URL
+  )
+) {
+  console.log(
+    "Please include ALCHEMY_API_KEY_URL, PRIVATE_KEY, and ETHERSCAN_API_KEY as env variables."
+  );
+  process.exit(1);
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      forking: {
+        //@ts-ignore
+        url: MAINET_ALCHEMY_API_KEY_URL,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      //@ts-ignore
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
 
 export default config;
